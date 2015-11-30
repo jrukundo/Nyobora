b354bd1e2e05639c8258ae7b03794806e076fed5
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:16:07 +0200

    Create CreateAnEvent.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/usergeneratedcontent/CreateAnEvent.java
@@ -0,0 +1,16 @@
+package com.school.ecallowa.nyobora.usergeneratedcontent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.school.ecallowa.nyobora.R;
+
+public class CreateAnEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_an_event);
+    }
+    
+}
