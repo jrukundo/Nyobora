65d71b752fedbf682b3fb1d53c4567cc9a801a40
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:03:23 +0200

    Create Saved.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/localcontent/Saved.java
@@ -0,0 +1,15 @@
+package com.school.ecallowa.nyobora.localcontent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.school.ecallowa.nyobora.R;
+
+public class Saved extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_info);
+    }
+}
