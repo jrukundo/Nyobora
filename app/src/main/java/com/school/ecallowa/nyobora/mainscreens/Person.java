573354d77396d0e3c6dd0917ad2d4e157adf5deb
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 21:50:50 +0200

    Update and rename People.java to Person.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/mainscreens/Person.java
@@ -0,0 +1,52 @@
+package com.school.ecallowa.nyobora.mainscreens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.school.ecallowa.nyobora.R;
+import com.school.ecallowa.nyobora.person.Forum;
+import com.school.ecallowa.nyobora.person.PersonsAround;
+import com.school.ecallowa.nyobora.person.Profile;
+
+public class Person extends AppCompatActivity {
+
+    Button forumButton,peopleAroundButton,myProfileButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+
+        forumButton = (Button) findViewById(R.id.forumButton);
+        peopleAroundButton = (Button) findViewById(R.id.peopleAreaButton);
+        myProfileButton = (Button) findViewById(R.id.profileButton);
+
+        forumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(getApplicationContext(),Forum.class);
+                startActivity(i);
+            }
+        });
+
+        peopleAroundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PersonsAround.class);
+                startActivity(i);
+            }
+        });
+        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(getApplicationContext(),Profile.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
