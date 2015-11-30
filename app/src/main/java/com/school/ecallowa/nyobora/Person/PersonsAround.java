4593ba936c375755702485fddbddce42205786c0
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:05:41 +0200

    Create PersonsAround.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/Person/PersonsAround.java
@@ -0,0 +1,16 @@
+package com.school.ecallowa.nyobora.person;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.school.ecallowa.nyobora.R;
+
+public class PersonsAround extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_around);
+    }
+    
+}
