aa733a44a5e1acd8589fe73a6865d70a037c501d
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:05:12 +0200

    Create Forum.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/Person/Forum.java
@@ -0,0 +1,22 @@
+package com.school.ecallowa.nyobora.person;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.school.ecallowa.nyobora.R;
+import com.school.ecallowa.nyobora.mainscreens.Language;
+import com.school.ecallowa.nyobora.mainscreens.MainScreen;
+import com.school.ecallowa.nyobora.mainscreens.Person;
+import com.school.ecallowa.nyobora.mainscreens.Settings;
+import com.school.ecallowa.nyobora.mainscreens.ThingsToDo;
+
+public class Forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+    }
+}
