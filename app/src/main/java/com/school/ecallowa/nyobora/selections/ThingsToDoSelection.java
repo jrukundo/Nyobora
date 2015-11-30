44017db5f9c79bad7e6788a9ac37fe2bedf6010b
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:15:22 +0200

    Create ThingsToDoSelection.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/selections/ThingsToDoSelection.java
@@ -0,0 +1,16 @@
+package com.school.ecallowa.nyobora.selections;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.school.ecallowa.nyobora.R;
+
+public class ThingsToDoSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_things_to_do_selection);
+    }
+
+}
