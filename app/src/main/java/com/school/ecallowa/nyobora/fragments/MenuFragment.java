b9ecf89a5bd14f24acc8201d95036dc38c67d0fa
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-11-30 22:01:48 +0200

    Update MenuFragment.java

--- app/src/main/java/com/school/ecallowa/nyobora/fragments/MenuFragment.java
+++ app/src/main/java/com/school/ecallowa/nyobora/fragments/MenuFragment.java
@@ -1,100 +1,107 @@
 package com.school.ecallowa.nyobora.fragments;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.school.ecallowa.nyobora.R;
+import com.school.ecallowa.nyobora.mainscreens.Language;
+import com.school.ecallowa.nyobora.mainscreens.MainScreen;
+import com.school.ecallowa.nyobora.mainscreens.Person;
+import com.school.ecallowa.nyobora.mainscreens.Settings;
+import com.school.ecallowa.nyobora.mainscreens.ThingsToDo;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MenuFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MenuFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuFragment newInstance(String param1, String param2) {
-        MenuFragment fragment = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public MenuFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View view =  inflater.inflate(R.layout.fragment_menu, container, false);
+
+            ImageButton b1 = (ImageButton) view.findViewById(R.id.homeButton);
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getHomePage(v);
+                }
+            });
+
+            ImageButton b2 = (ImageButton) view.findViewById(R.id.settingsMenuButton);
+            b2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getSettingsPage(v);
+                }
+            });
+
+            Button b3 = (Button) view.findViewById(R.id.peopleButton);
+            b3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getPeoplePage(v);
+                }
+            });
+
+            Button b4 = (Button) view.findViewById(R.id.ttdButton);
+            b4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getThingsToDoPage(v);
+                }
+            });
+
+            Button b5 = (Button) view.findViewById(R.id.langButton);
+            b5.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getLanguagePage(v);
+                }
+            });
+
+            return view;
         }
+
+    public void getPeoplePage(View v){
+        Intent intent = new Intent(getActivity(), Person.class);
+        startActivity(intent);
+
+    }
+    public void getThingsToDoPage(View v){
+
+        Intent intent = new Intent(getActivity(), ThingsToDo.class);
+        startActivity(intent);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+    public void getLanguagePage(View v){
+
+        Intent intent = new Intent(getActivity(), Language.class);
+        startActivity(intent);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void getHomePage(View v){
+
+        Intent intent = new Intent(getActivity(), MainScreen.class);
+        startActivity(intent);
+    }
+    public void getSettingsPage(View v){
+        Intent intent = new Intent(getActivity(),Settings.class);
+        startActivity(intent);
     }
 
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
 
 }
+
