20141d09b97e2da3ffd4ccc830125a6e33752b97
ebonycalloway <ebonycalloway2015@u.northwestern.edu>
2015-12-13 18:48:49 +0200

    Create CreatePerson.java

---
+++ app/src/main/java/com/school/ecallowa/nyobora/adapters/CreatePerson.java
@@ -0,0 +1,10 @@
+package com.school.ecallowa.nyobora.adapters;
+
+import android.content.Context;
+
+/**
+ * Created by ecalloway on 12/6/2015.
+ */
+public interface CreatePerson {
+    public void newPerson(Context context);
+}
